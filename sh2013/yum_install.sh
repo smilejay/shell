55757e76d5a90669a49a8afeff20a674290634a2
Jay <smile665@gmail.com>
2017-08-15 15:00:12 +0800

    update yum_install script

--- sh2013/yum_install.sh
+++ sh2013/yum_install.sh
@@ -3,41 +3,20 @@
 # BTW, when using 'yum install' you can use '-y' option answer any questions with 'yes'.
 # -y, --assumeyes
 #              Assume yes; assume that the answer to any question which would be asked is yes.
+webserver=nginx  # or httpd
 
-yum install vim
-yum install openssh*
-yum install openssh-client
-yum install httpd
-yum install mysql
-yum install mysql-server
-yum install php
-yum install wget
-yum install php-mysql
-yum install iptraf
-yum install iftraf
-yum install iftop
-yum install sysstat
-yum install git
-yum install bc
-yum install mailx
-yum install postfix
+yum install -y openssh* openssh-clients
+yum install -y $webserver mysql mysql-server php wget php-mysql
+yum install -y vim iptraf iftraf iftop sysstat git bc mailx postfix
 
 # use 'system-config-firewall-tui' to config firewall
-yum install system-config-firewall-tui
+yum install -y system-config-firewall-tui
 
 
 # add more packages which have nothing to do with WordPress.
-yum install pciutils
-yum install svn
-yum install glibc-common
-yum install ntpdate
-yum install gcc
-yum install make
-yum install freetype
-yum install fontconfig
+yum install -y pciutils svn glibc-common ntpdate gcc make freetype fontconfig
 
 # for nginx
 # http://centos.alt.ru/repository/centos/6/x86_64/nginx-1.5.8-1.el6.x86_64.rpm
-# http://www6.atomicorp.com/channels/atomic/centos/6/x86_64/RPMS/GeoIP-1.4.8-1.1.el6.art.x86_64.rpm 
-yum install openssl
-yum install php-fpm
+# http://www6.atomicorp.com/channels/atomic/centos/6/x86_64/RPMS/GeoIP-1.4.8-1.1.el6.art.x86_64.rpm
+yum install -y openssl php-fpm
