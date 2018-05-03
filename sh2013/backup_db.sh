10b860f0e5e94d78a610872600c4cbbb7cb26a49
Jay <smile665@gmail.com>
2018-05-03 20:45:16 +0800

    optimize backup_db.sh

--- sh2013/backup_db.sh
+++ sh2013/backup_db.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
 #author: smile665@gmail.com
 #filename: backup_db.sh
-# to back up some databases for a QA system
+# to back up some databases
 
 backup=/home/backup_db
-databases=" testlink mysql information_schema"
+databases="testlink dashboard"
 reserve_days=30
 log_file=backup_db.log
 log_msg=""
 msg_head="["$(date +"%F %T")"]"
+db_pass='123123'
 
 
 #create log file
 function create_log()
 {
+	if [ ! -f $log_file ]; then
+		logsize=0
+	else
+		logsize=$(du -sm $log_file|awk '{print $1}')
+	fi
 	logsize=$(du -sm $log_file|awk '{print $1}')
 	if [ $logsize -gt 1024 ];then
 		cp -i $log_file ${log_file}_bk
-		cat /dev/null > $log_file     
+		cat /dev/null > $log_file
 	fi
-	echo -e $log_msg >> $log_file
+	echo -e $log_msg | tee -a $log_file
 }
 
 #dump a database
 function dump_database()
 {
-	#quick dump database and add lock when duming
-	mysqldump --databases $1 --user=root --password= --max_allowed_packet=40960M -q -c -R>$filename.sql
+	local dbname=$1
+	local filename=$2
+	mysqldump --databases $dbname --user=root --password=$db_pass --max_allowed_packet=40960M -q -c -R>$filename.sql
 	if [ $? -eq 0 ];then
 		log_msg="$log_msg$msg_head : OK: dump database $1 successfully \n"
-		sleep 10
+		sleep 1
 		return 0
 	else
 		log_msg="$log_msg$msg_head : Fail: failed to dump database $db \n"
-		sleep 10
+		sleep 1
 		return 1
 	fi
 }
@@ -76,7 +83,7 @@ function remove_oldfile()
 function cd_backup()
 {
 	if [ -d $backup ];then
-		chattr -i $backup 2>/dev/nul
+		chattr -i $backup 2>/dev/null
 	else
 		mkdir -p $backup
 	fi
@@ -93,6 +100,6 @@ do
 	compress_backupfile $filename
 done
 remove_oldfile
-chattr +i $backup
 log_msg="$log_msg------backup_db.sh ends at $(date)----------------------------------\n"
 create_log
+chattr +i $backup
